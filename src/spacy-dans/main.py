959c5d71bc20c8ca7339a01299972f8146150745
ekoi <eko.indarto@dans.knaw.nl>
2022-03-29 05:53:36 +0200

    Intermediate commit: Upload pdf file.

--- src/spacy-dans/main.py
+++ src/spacy-dans/main.py
@@ -5,7 +5,7 @@
 import uvicorn
 import pandas as pd
 from DatesRecognition import DatesRecognition
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pyDataverse.api import Api, NativeApi
@@ -35,6 +35,10 @@ from readabilipy import simple_json_from_html_string
 #import codecs
 import datefinder
 from datetime import datetime
+
+from parsr_client import ParsrClient
+
+parsr = ParsrClient('localhost:3001')
 
 def custom_openapi():
     if app.openapi_schema:
@@ -124,6 +128,30 @@ async def dataverse(baseurl: str, doi: str, token: Optional[str] = None):
         return 'Error: no entities found in metadata'
     else:
         return 'Error: no metadata found'
+
+
+# curl -X POST -F "file=@/Users/akmi/git/CLARIAH/Parsr/samples/ekoi.pdf" http://localhost:9266/upload
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    contents = await file.read()
+    save_file(file.filename, contents)
+    parsr.send_document(file_path="../../tmp/" + file.filename, config_path="./conf/defaultConfig.json", document_name=file.filename, save_request_id=True)
+    # if result_type == 'JSON':
+    #     return parsr.get_json()
+    # elif result_type == "MARKDOWN":
+    #     return parsr.get_markdown()
+    # elif result_type == "TEXT":
+    #     return parsr.get_text()
+    # else:
+    #     return None
+    x = parsr.get_text()
+    print(x)
+    return 200
+
+
+def save_file(filename, data):
+    with open("../../tmp/" + filename, 'wb') as f:
+        f.write(data)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9266)
